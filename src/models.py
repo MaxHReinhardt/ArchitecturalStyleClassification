e9528e2aae9dcacae2ff8ce4e0f281ce4cd4b7eb
MaxHReinhardt <max.reinhardt.hh@gmail.com>
2023-12-14 23:51:19 +0100

    Integrate CBAM before softmax classifier

--- src/models.py
+++ src/models.py
@@ -1,8 +1,76 @@
+import torch
 import torch.nn as nn
 
 
+class ChannelAttentionModule(nn.Module):
+    def __init__(self, ch):
+        super(ChannelAttentionModule, self).__init__()
+
+        ratio = 16
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(ch, ch//ratio, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(ch//ratio, ch, bias=False)
+        )
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x1 = self.avg_pool(x)
+        x1 = self.mlp(x1.view(x1.size(0), -1))
+        x1 = x1.view(x1.size(0), x1.size(1), 1, 1)
+
+        x2 = self.max_pool(x)
+        x2 = self.mlp(x2.view(x2.size(0), -1))
+        x2 = x2.view(x2.size(0), x2.size(1), 1, 1)
+
+        feats = x1 + x2
+        feats = self.sigmoid(feats)
+        refined_feats = x * feats
+
+        return refined_feats
+
+
+class SpatialAttentionModule(nn.Module):
+    def __init__(self):
+        super(SpatialAttentionModule, self).__init__()
+
+        kernel_size = 7
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=3, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x1 = torch.mean(x, dim=1, keepdim=True)
+        x2, _ = torch.max(x, dim=1, keepdim=True)
+
+        feats = torch.cat([x1, x2], dim=1)
+        feats = self.conv(feats)
+        feats = self.sigmoid(feats)
+        refined_feats = x * feats
+
+        return refined_feats
+
+
+class CbamIntegration(nn.Module):
+    def __init__(self, channel):
+        super(CbamIntegration, self).__init__()
+
+        self.ca = ChannelAttentionModule(channel)
+        self.sa = SpatialAttentionModule()
+
+    def forward(self, x):
+        residual = x
+        x = self.ca(x)
+        x = self.sa(x)
+        out = x + residual
+        return out
+
+
 class MobileNetV1(nn.Module):
-    def __init__(self, ch_in, n_classes):
+    def __init__(self, ch_in, n_classes, with_cbam=False):
         super(MobileNetV1, self).__init__()
 
         # Define standard convolution with batch norm
@@ -27,7 +95,7 @@ class MobileNetV1(nn.Module):
                 nn.ReLU(inplace=True),
                 )
 
-        self.model = nn.Sequential(
+        self.convolutional_layers = nn.Sequential(
             conv_bn(ch_in, 32, 2),
             conv_dw(32, 64, 1),
             conv_dw(64, 128, 2),
@@ -41,16 +109,23 @@ class MobileNetV1(nn.Module):
             conv_dw(512, 512, 1),
             conv_dw(512, 512, 1),
             conv_dw(512, 1024, 2),
-            conv_dw(1024, 1024, 1),
-            nn.AdaptiveAvgPool2d(1)
+            conv_dw(1024, 1024, 1)
         )
-        self.fc = nn.Linear(1024, n_classes)
+
+        self.with_cbam = with_cbam
+
+        self.cbam_last_layer = CbamIntegration(1024)
+
+        self.final_pool = nn.AdaptiveAvgPool2d(1)
+
+        self.final_linear = nn.Linear(1024, n_classes)
 
     def forward(self, x):
-        x = self.model(x)
+        x = self.convolutional_layers(x)
+        if self.with_cbam:
+            x = self.cbam_last_layer(x)
+        x = self.final_pool(x)
         x = x.view(-1, 1024)
-        x = self.fc(x)
+        x = self.final_linear(x)
         return x
 
-
-# TODO: Write MobileNetV1 + CBAM model class
--- tests/test_training_and_evaluation.py
+++ tests/test_training_and_evaluation.py
@@ -53,7 +53,6 @@ def test_training_for_n_epochs_and_evaluation():
     print(f"Accuracy: {accuracy}, Macro F1: {macro_f1}, Average Loss: {avg_loss}")
 
 
-# TODO: Apply augmentation only on training not on validation data and find a good solution for resizing operations
 def test_training_with_early_stopping():
     model = MobileNetV1(ch_in=3, n_classes=25)
 
@@ -103,3 +102,48 @@ def test_training_with_early_stopping():
     print(f"train_loss_development: {train_loss_development}")
     print(f"val_loss_development: {val_loss_development}")
 
+
+def test_cbam_integration():
+    model = MobileNetV1(ch_in=3, n_classes=25, with_cbam=True)
+
+    train_transforms = v2.Compose(
+        [
+            # Convert to image tensor with datatype uint8
+            v2.ToImage(),
+            v2.ToDtype(torch.uint8, scale=True),
+
+            # Augment data
+            v2.RandomResizedCrop(size=(120, 120), antialias=True),
+            v2.RandomHorizontalFlip(p=0.5),
+            v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=None),
+            v2.RandomRotation(degrees=15),
+            # v2.RandomAffine(degrees=0, translate=(0.1, 0.1)),  # Apply random affine transformations
+            # v2.RandomGrayscale(p=0.1),
+
+            # Convert to float32 and normalize
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize with ImageNet mean and SD
+        ]
+    )
+
+    train_set = ArchitecturalStylesDataset(
+        csv_file="data/dataset/train_annotation_tiny.csv",
+        transform=train_transforms,
+    )
+
+    batch_size = 8
+    learning_rate = 0.003
+    num_epochs = 2
+
+    # Check if CUDA (GPU) is available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Device: {device}")
+    model.to(device)
+
+    trained_model, train_loss_development = train_for_n_epochs(model, train_set, batch_size, learning_rate, num_epochs, device)
+
+    print(f"Train loss development : {train_loss_development}")
+
+    # Use train set also for evaluation for testing
+    accuracy, macro_f1, avg_loss = evaluate(model, train_set, batch_size, device)
+    print(f"Accuracy: {accuracy}, Macro F1: {macro_f1}, Average Loss: {avg_loss}")
